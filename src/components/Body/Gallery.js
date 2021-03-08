cf2401a718dd1683b6dc721a2b845b00d4870a53
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-03-08 12:28:19 -0800

    front end complete

--- src/components/Body/Gallery.js
+++ src/components/Body/Gallery.js
@@ -1,42 +1,46 @@
 import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
-import "./Gallery.css";
+import "./../style/Gallery.css";
 const Gallery = () => {
   const [photos, setPhotos] = useState([
     {
       name: "firstPerson",
-      description: "not sure",
       url:
         "https://www.biography.com/.image/c_fill%2Ccs_srgb%2Cfl_progressive%2Ch_400%2Cq_auto:good%2Cw_620/MTE4MDAzNDEwMzY0NDMzOTM0/jessica-alba-299896-1-402.jpg",
     },
     {
       name: "SecondPerson",
-      description: "not sure",
       url:
         "https://c8.alamy.com/comp/P1KE01/photoshop-vector-icon-isolated-on-transparent-background-photoshop-logo-concept-P1KE01.jpg",
     },
   ]);
   const onSwipe = (direction) => {
-    console.log("You swiped: " + direction);
+    // console.log("You swiped: " + direction);
   };
 
   const onCardLeftScreen = (myIdentifier) => {
-    console.log(myIdentifier + " left the screen");
+    // console.log(myIdentifier + " left the screen");
   };
   return (
-    <div className="gallery">
-      {photos.map((photo) => (
-        <TinderCard
-          onSwipe={onSwipe}
-          onCardLeftScreen={() => onCardLeftScreen("fooBar")}
-          preventSwipe={["right", "left"]}
-        >
-          <div className="card">
-            <span className="photo-name">{photo.name}</span>
-            <img src={photo.url} alt="photo" />
-          </div>
-        </TinderCard>
-      ))}
+    <div className="Gallery">
+      <div className="cardContainer">
+        {photos.map((photo) => (
+          <TinderCard
+            key={photo.name}
+            className="swipe"
+            onSwipe={onSwipe}
+            onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+            preventSwipe={["up", "down"]}
+          >
+            <div
+              className="img"
+              style={{ backgroundImage: `url(${photo.url})` }}
+            >
+              <span className="photo-name">{photo.name}</span>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,31 +2,34 @@ import React from "react";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
-import "./Header.css";
-import logo from "./logo.png";
+import "./../style/Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       {/* <AccountCircle /> */}
       {/* <AccountCircleIcon /> */}
-      <IconButton>
-        <a href="https://google.com">
-          <AccountCircleIcon fontSize="Large" />
-        </a>
-      </IconButton>
+      <div className="btn">
+        <IconButton>
+          <a href="https://google.com">
+            <AccountCircleIcon fontSize="large" />
+          </a>
+        </IconButton>
+      </div>
 
-      <div className="">
+      <div className="logo">
         <a href="https://google.com">
-          <img className="header_logo" src={logo} alt="photo" />
+          <b>logo</b>
         </a>
       </div>
 
-      <IconButton>
-        <a href="https://google.com">
-          <MenuIcon fontSize="large" />
-        </a>
-      </IconButton>
+      <div className="btn">
+        <IconButton>
+          <a href="#">
+            <MenuIcon fontSize="large" />
+          </a>
+        </IconButton>
+      </div>
     </div>
   );
 };
